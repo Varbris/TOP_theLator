6d0d415709d79ee79173a3c36fd44e17d42b0b73
Varbris <92345143+Varbris@users.noreply.github.com>
2025-02-15 13:53:03 +0700

    Add lator ui to html

--- thejava.js
+++ thejava.js
@@ -9,11 +9,40 @@ function divide(num) {}
 function operate(operator, num1, num2) {}
 
 function addNumberButton() {
-  const button = document.createElement("button");
   let arrButton = [];
-  for (let i = 0; i <= 9; i++) {
-    arrButton.push(button);
+  for (let i = 9; i >= 0; i--) {
+    const button = document.createElement("button");
     button.innerText = `${i}`;
+    button.setAttribute("id", `button${i}`);
+    button.classList.add("numButton");
+    arrButton.push(button);
   }
   return arrButton;
 }
+function addOperatorButton() {
+  const arrOperator = ["/", "*", "-", "+", "="];
+  let arrButton = [];
+  for (let i = 0; i < arrOperator.length; i++) {
+    const button = document.createElement("button");
+    button.innerText = `${arrOperator[i]}`;
+    button.setAttribute("id", `button${arrOperator[i]}`);
+    button.classList.add("operatorButton");
+    arrButton.push(button);
+  }
+  return arrButton;
+}
+
+function generateButton() {
+  const numberContainer = document.querySelector(".number-container");
+  const operatorContainer = document.querySelector(".operator-container");
+  const arrNumButton = addNumberButton();
+  const arrOperatorButton = addOperatorButton();
+  arrNumButton.forEach(function (item) {
+    numberContainer.appendChild(item);
+  });
+  arrOperatorButton.forEach(function (item) {
+    operatorContainer.appendChild(item);
+  });
+}
+
+generateButton();
