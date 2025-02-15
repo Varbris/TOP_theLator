6bd3a35972ac9fd3ad548324a5d0d3256cb8cd8a
Varbris <92345143+Varbris@users.noreply.github.com>
2025-02-15 11:20:41 +0700

    Add numberGenerator function to javascript

--- thejava.js
+++ thejava.js
@@ -7,3 +7,13 @@ function multiply(num) {}
 function divide(num) {}
 
 function operate(operator, num1, num2) {}
+
+function addNumberButton() {
+  const button = document.createElement("button");
+  let arrButton = [];
+  for (let i = 0; i <= 9; i++) {
+    arrButton.push(button);
+    button.innerText = `${i}`;
+  }
+  return arrButton;
+}
